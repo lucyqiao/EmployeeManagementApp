85403bbd15aac8121c52c0589f88bea862ce50b3
QiaoQian <LucyQiao@qianqaos-MacBook-Air.local>
2016-03-31 13:02:32 -0700

    add image upload

--- public/js/myApp.js
+++ public/js/myApp.js
@@ -1,4 +1,4 @@
-angular.module('myApp', ['ngRoute'])
+angular.module('myApp', ['ngRoute','ngFileUpload'])
 //routing config
 .config(['$routeProvider',
     function($routeProvider) {
@@ -42,35 +42,60 @@ angular.module('myApp', ['ngRoute'])
         getOneEmployee : function(id) {
             return $http.get('/employees/' + id.toString());
         },
-        updateEmployee : function(employee) {
-            return $http.put('/employees/' + employee._id.toString(), employee);
-        },
         deleteEmployee : function(id) {
             return $http.delete('/employees/' + id.toString());
+        },
+        updateEmployee : function(employee) {
+            return $http.put('/employees/' + employee._id.toString(), employee);
         }
     };
 }])
 
-//edit employee page
-.controller('EditEmployeeCtrl', function($scope, $routeParams, $timeout, $location, employeeFactory) {
-    employeeFactory.getOneEmployee($routeParams.id)
-        .then(function(res) {
-            $scope.employee = res.data;
-        });
-    employeeFactory.getEmployees()
-        .then(function(res){
-            $scope.employees = res.data;
-        });
-    $scope.updateEmployee = function() {
-        employeeFactory.updateEmployee($scope.employee)
+    .controller('EditEmployeeCtrl', function($scope, $routeParams, Upload, $timeout, $location, employeeFactory) {
+        employeeFactory.getOneEmployee($routeParams.id)
             .then(function(res) {
-                console.log("Updated"+ res);
-                $location.path("/");
-            }, function(err) {
-                console.log(err);
+                $scope.employee = res.data;
             });
-    };
-})
+        employeeFactory.getEmployees()
+            .then(function(res){
+                $scope.employees = res.data;
+            });
+        $scope.updateEmployee = function() {
+            employeeFactory.updateEmployee($scope.employee)
+                .then(function(res) {
+                    console.log("Updated"+ res);
+                    $location.path("/");
+                }, function(err) {
+                    console.log(err);
+                });
+        };
+        $scope.uploadFiles = function(file, errFiles) {
+            $scope.f = file;
+            $scope.errFile = errFiles && errFiles[0];
+            if (file) {
+                file.upload = Upload.upload({
+                    url: '/upload',
+                    method: 'POST',
+                    data : {name: $scope.employee.name.replace(' ', '')},
+                    file: file
+                });
+
+                file.upload.then(function (response) {
+                    $timeout(function () {
+                        file.result = response.data;
+                    });
+                }, function (response) {
+                    if (response.status > 0) {
+                        $scope.errorMsg = response.status + ': ' + response.data;
+                        console.log($scope.errorMsg);
+                    }
+                }, function (evt) {
+                    file.progress = Math.min(100, parseInt(100.0 *
+                        evt.loaded / evt.total));
+                });
+            }
+        };
+    })
 
 //employee direct reports page
 .controller('DirReportsCtrl', function($scope, $routeParams, employeeFactory) {
@@ -109,29 +134,59 @@ angular.module('myApp', ['ngRoute'])
 
 })
 //new employee controller
-.controller('NewEmployeeCtrl', function($scope, $location, employeeFactory) {
-    $scope.addEmployee = function() {
-        var employee = {
-            name : $scope.name,
-            title : $scope.title,
-            sex   : $scope.sex,
-            startDate : $scope.startDate,
-            officePhone : $scope.officePhone,
-            cellPhone : $scope.cellPhone,
-            email : $scope.email,
-            manager : $scope.manager
-        };
-        employeeFactory.addEmployee(employee)
-            .then(function(res) {
-                $location.path("/");
-                console.log(res);
-            }, function(err) {
-                $location.path("/");
-                console.log(err);
+    .controller('NewEmployeeCtrl', function($scope, $location, Upload, $timeout, employeeFactory) {
+        employeeFactory.getEmployees()
+            .then(function(res){
+                $scope.employees = res.data;
             });
+        $scope.addEmployee = function() {
+            var employee = {
+                name : $scope.fName + " " + $scope.lName,
+                title : $scope.title,
+                sex   : $scope.sex,
+                startDate : $scope.startDate,
+                officePhone : $scope.officePhone,
+                cellPhone : $scope.cellPhone,
+                email : $scope.email
+            };
+            if($scope.manager && ($scope.manager != 'None')) employee.manager = $scope.manager;
+            employeeFactory.addEmployee(employee)
+                .then(function(res) {
+                    $location.path("/");
+                    console.log(res);
+                }, function(err) {
+                    $location.path("/");
+                    console.log(err);
+                });
 
-    }
-})
+        };
+        $scope.uploadFiles = function(file, errFiles) {
+            $scope.f = file;
+            $scope.errFile = errFiles && errFiles[0];
+            if (file) {
+                file.upload = Upload.upload({
+                    url: '/upload',
+                    method: 'POST',
+                    data : {name: $scope.fName + $scope.lName},
+                    file: file
+                });
+
+                file.upload.then(function (response) {
+                    $timeout(function () {
+                        file.result = response.data;
+                    });
+                }, function (response) {
+                    if (response.status > 0) {
+                        $scope.errorMsg = response.status + ': ' + response.data;
+                        console.log($scope.errorMsg);
+                    }
+                }, function (evt) {
+                    file.progress = Math.min(100, parseInt(100.0 *
+                        evt.loaded / evt.total));
+                });
+            }
+        };
+    })
 //employee list controller
 .controller('EmployeeListCtrl', function($scope,$location,employeeFactory) {
     employeeFactory.getEmployees()
